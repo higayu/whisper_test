e63c64700efb893e7219a5c09359ac0f5a7a3397
higashiyama <higashiyama2024fcc@gmail.com>
2025-08-27 14:58:58 +0900

    プライベートアドレスから送信OK

--- TestRequest.py
+++ TestRequest.py
@@ -1,7 +1,8 @@
 import requests
 
-url = "http://localhost:8000/transcribe"
-with open(r"C:\Users\Higashiyama\Documents\_python\ohayou.wav", "rb") as f:
+#url = "http://localhost:8081/transcribe"
+url = "http://192.168.1.221:8081/transcribe"
+with open(r"ohayou.wav", "rb") as f:
     files = {"file": ("ohayou.wav", f, "audio/wav")}
     r = requests.post(url, files=files)
 
--- henkan.py
+++ henkan.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment
 
-mp3_path = r"C:\Users\Higashiyama\Documents\_python\ohayou.mp3"
-wav_path = r"C:\Users\Higashiyama\Documents\_python\ohayou.wav"
+mp3_path = r"ohayou.mp3"
+wav_path = r"ohayou.wav"
 
 sound = AudioSegment.from_mp3(mp3_path)
 sound.export(wav_path, format="wav")
